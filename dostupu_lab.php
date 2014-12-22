087b9eb7d582b987d122752ba735392571c261ea
OsmanCrimea <o.cholakchik@gmail.com>
2014-12-22 12:31:15 +0300

    Насало работы

---
+++ dostupu_lab.php
@@ -0,0 +1,28 @@
+Новый хостигш
+IP: 37.120.170.23
+Root-Passwort: zCQYbem6wUqcf2Y
+[06.11.2014 10:06:57] Osman: а какой логин пользователя?
+[06.11.2014 10:07:14] Nikita: используй sftp
+[06.11.2014 10:07:18] Nikita: root
+
+____________
+bionlab.ru/admin
+osman
+zTwOdiV12
+_______________
+bionlab ftp
+[11:18:51] Nikita: x3jnflh9
+Имя пользователя (логин): bionlab_ftp_1
+Адрес FTP-сервера: ftp.bionlab.nichost.ru
+_______________________
+https://bitbucket.org/
+[20.11.2014 12:39:38] Osman: o.cholakchik@gmail.com
+[20.11.2014 12:41:14] tralyalyalyalya: FTP: 37.221.192.11 lab4u_ftp Jeeg9eib
+[20.11.2014 12:45:56] tralyalyalyalya: https://netbeans.org/downloads/
+[20.11.2014 12:47:51] tralyalyalyalya: https://bitbucket.org/lab4u_ru/lab4u_ru_site.git
+[20.11.2014 12:52:48] tralyalyalyalya: http://git-scm.com/book/ru/v1
+[20.11.2014 13:06:57] tralyalyalyalya: http://dev.lab4u.ru noauth y4Lcn'tView
+o.cholakchik@gmail.com email на dev
+[20.11.2014 13:10:50] Osman: Спасибо!
+[20.11.2014 13:11:18 | Изменены 13:11:24] tralyalyalyalya: Админ. Выйдите, зайдите.
+[20.11.2014 13:11:39] tralyalyalyalya: http://dev.lab4u.ru/bitrix/admin/
